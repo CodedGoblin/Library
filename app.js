918968c971166172a3fffd2daee48c10e53ed9da
CodedGoblin <athmane1naili@gmail.com>
2023-03-07 12:35:57 +0100

    Added Delete entry functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let myLibrary = [{title:"The Hobbit", author:'Tolkien', year:'1948', read:'read'}];
+let myLibrary = [{title:"The Hobbit", author:'Tolkien', year:'1948', read:'read', bookId: 0}];
 const table = document.querySelector('tbody');
 const form = document.querySelector('form');
 const inpTitle = document.querySelector('#title');
@@ -10,27 +10,30 @@ const saveBook = document.querySelector('form button');
 
 
 
-function Book(title,author,pages,read){
+
+function Book(title,author,pages,read, bookId){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
+    this.bookId = bookId;
 }
 
-function addBookToLibrary(){
-    let bookTitle = inpTitle.value;
-    let bookAuthor = inpAuthor.value;
-    let bookYear = inpYear.value;
-    let bookRead = inpStatus.value;
+let id = 0; 
 
-    const book = new Book(bookTitle, bookAuthor, bookYear, bookRead);
-    myLibrary.push(book);
+function addBookToLibrary(lib){
+    const bookTitle = inpTitle.value;
+    const bookAuthor = inpAuthor.value;
+    const bookYear = inpYear.value;
+    const bookRead = inpStatus.value;
+    const bookId = id+1;
+    const book = new Book(bookTitle, bookAuthor, bookYear, bookRead, bookId);
+    lib.push(book);
 }
 
 
-function fillTable(){
-    for(let book of myLibrary){
-        newRow();
+function fillTable(lib){
+    for(let book of lib){
         fillRow(book, newRow());
     }
 }
@@ -44,33 +47,48 @@ function newRow(){
 function fillRow(book, row){
     for(let key in book){
         if(book.hasOwnProperty(key)){
+            if(key == 'bookId') break;
             if(key == 'title' ) {
                 const tHead = document.createElement('th');
                 tHead.textContent = book[key];
                 row.append(tHead);
+                const X = document.createElement('button');
+                X.addEventListener('click', ()=>{
+                    document.querySelector(`[data-id="${X.dataset.id}"]`).remove();
+                    removal(X.dataset.id);
+                })
+                X.textContent = 'x';
+                X.dataset.id = book.bookId;
+                tHead.append(X);
                 continue;
             }
             const tData = document.createElement('td');
             tData.textContent = book[key];
             row.append(tData);
+            row.dataset.id = book.bookId;
         }
     }
 }
 
 
-fillTable();
+fillTable(myLibrary);
 
 newBook.addEventListener('click', ()=>{
     form.classList.remove('hide');
 })
 
-
 saveBook.addEventListener('click', (evt)=>{
     evt.preventDefault();
-    addBookToLibrary();
-    newRow();
+    addBookToLibrary(myLibrary);
     fillRow(myLibrary[myLibrary.length-1],newRow());
     form.classList.add('hide');
 })
+
+function removal(id){
+    const newLib = myLibrary.filter(book => !book.bookId == id);
+    myLibrary = newLib;
+    fillTable(myLibrary);
+}
+
 
 
